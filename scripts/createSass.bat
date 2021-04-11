0d11758e00ad6ec8c4162a2fe584ca511c36c6d0
matthew.french <matthew.french@derivco.co.uk>
2021-04-11 13:10:28 +0100

    add sass folders and about section

---
+++ scripts/createSass.bat
@@ -0,0 +1,28 @@
+mkdir .\sass
+touch .\sass\main.scss
+
+mkdir .\sass\base
+touch .\sass\base\_animations.scss
+touch .\sass\base\_base.scss
+touch .\sass\base\_typography.scss
+touch .\sass\base\_utilities.scss
+
+mkdir .\sass\abstracts
+touch .\sass\abstracts\_variables.scss
+touch .\sass\abstracts\_mixins.scss
+touch .\sass\abstracts\_functions.scss
+
+mkdir .\sass\components
+touch .\sass\components\.gitignore
+
+mkdir .\sass\layout
+touch .\sass\layout\.gitignore
+
+mkdir .\sass\pages
+touch .\sass\pages\_home.scss
+
+mkdir .\sass\themes
+touch .\sass\themes\.gitignore
+
+mkdir .\sass\architecture
+touch .\sass\architecture\.gitignore
